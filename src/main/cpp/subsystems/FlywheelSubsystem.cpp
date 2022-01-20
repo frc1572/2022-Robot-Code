cb8177f6b79511e0f0afb4eee2ad323250b1b91f
Connor Worley <connorbworley@gmail.com>
2022-01-19 18:30:27 -0800

    fix bug where Ks was not multiplied by sgn(voltage)

--- src/main/cpp/subsystems/FlywheelSubsystem.cpp
+++ src/main/cpp/subsystems/FlywheelSubsystem.cpp
@@ -15,14 +15,20 @@ void FlywheelSubsystem::Periodic() {
     rad_per_s_t velocity = m_leader.GetSelectedSensorVelocity() / Constants::TicksPerRevolution::TalonFX / Constants::VelocityFactor::TalonFX;
     m_loop.Correct(frc::MakeMatrix<1, 1>(velocity.to<double>()));
     m_loop.Predict(Constants::LoopPeriod);
-    m_leader.SetVoltage(Constants::Flywheel::Ks + m_loop.U(0) * 1_V);
+    auto voltage = m_loop.U(0);
+    m_leader.SetVoltage(voltage * 1_V + wpi::sgn(voltage) * Constants::Flywheel::Ks);
 
     frc::SmartDashboard::PutNumber("Flywheel.MeasuredState", velocity.to<double>());
     frc::SmartDashboard::PutNumber("Flywheel.EstimatedState", m_loop.Xhat(0));
 }
 
 void FlywheelSubsystem::SimulationPeriodic() {
-    m_plantSim.SetInput(0, m_leader.GetMotorOutputVoltage() - Constants::Flywheel::Ks / 1_V);
+    auto motorVoltage = m_leader.GetMotorOutputVoltage() * 1_V;
+    auto systemVoltage = motorVoltage - wpi::sgn(motorVoltage) * Constants::Flywheel::Ks;
+    if (systemVoltage > -Constants::Flywheel::Ks && systemVoltage < Constants::Flywheel::Ks) {
+        systemVoltage = 0_V;
+    }
+    m_plantSim.SetInput(0, systemVoltage / 1_V);
     m_plantSim.Update(Constants::LoopPeriod);
     auto output = m_plantSim.GetOutput(0) * 1_rad_per_s * Constants::TicksPerRevolution::TalonFX;
     auto leaderSim = m_leader.GetSimCollection();
