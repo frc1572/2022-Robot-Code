2bbd937046dc6a4f532d55ee4e548894fb01ad91
Stephen Maule <stephenmaule77@gmail.com>
2022-01-20 02:20:49 -0800

    Changed Values In Constant.h To Match Tested Values & Updated LeaderID

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -9,10 +9,10 @@ namespace Constants {
     constexpr auto LoopPeriod = 1_s / 100;
 
     namespace Flywheel {
-        constexpr int LeaderID = 0;
-        constexpr Ks_t Ks = 1_V;
-        constexpr Kv_t<units::radian_t> Kv = 0.025_V / 1_rad_per_s;
-        constexpr Ka_t<units::radian_t> Ka = 0.0025_V / 1_rad_per_s_sq;
+        constexpr int LeaderID = 1;
+        constexpr Ks_t Ks = 0.93373_V;
+        constexpr Kv_t<units::radian_t> Kv = 0.018794_V / 1_rad_per_s;
+        constexpr Ka_t<units::radian_t> Ka = 0.0014677_V / 1_rad_per_s_sq;
     }
 
     namespace TicksPerRevolution {
