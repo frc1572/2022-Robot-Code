d0ccedf25460bb0aa357108665dc2fa780f6acc1
Connor Worley <connorbworley@gmail.com>
2022-03-16 17:13:54 -0700

    reduce spdlog usage?

--- src/main/cpp/helper/spdlog.cpp
+++ src/main/cpp/helper/spdlog.cpp
@@ -19,21 +19,21 @@ void handleSpdlogError(const std::string& msg)
 
 void setupSpdlog()
 {
-    std::string operatingDirectory = frc::filesystem::GetOperatingDirectory();
+    // std::string operatingDirectory = frc::filesystem::GetOperatingDirectory();
 
     try
     {
-        spdlog::init_thread_pool(256, 1);
+        spdlog::init_thread_pool(256, 2);
         spdlog::flush_every(std::chrono::seconds(1));
 
-        auto rotatingFileLogger = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
-            operatingDirectory + "/logs/spdlog.log", 1024 * 1024, 10);
+        // auto rotatingFileLogger = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+        //     operatingDirectory + "/logs/spdlog.log", 1024 * 1024, 10);
         auto stdoutLogger = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
         auto logger = std::make_shared<spdlog::async_logger>(
             "spdlog",
             spdlog::sinks_init_list{
-                rotatingFileLogger,
+                // rotatingFileLogger,
                 stdoutLogger,
             },
             spdlog::thread_pool(),
