210d50992878fad9fce1350c034ba8884a201bf1
Connor Worley <connorbworley@gmail.com>
2022-03-31 11:26:19 -0700

    clean up auto-aim inertia

--- src/main/cpp/commands/AutoTurretCommand.cpp
+++ src/main/cpp/commands/AutoTurretCommand.cpp
@@ -29,16 +29,16 @@ frc::Rotation2d AutoTurretCommand::CalculateDesiredAngle()
 {
     auto pose = m_drivetrain.GetPose();
     auto goalOffset = Constants::GoalTranslation - pose.Translation();
-    auto goalDistance = goalOffset.Norm();
 
-    auto poseChange = pose.Translation() - m_previousPose.Translation();
-    auto dx = poseChange.X() / Constants::LoopPeriod;
-    auto dy = poseChange.Y() / Constants::LoopPeriod;
     auto flywheelVelocity = m_flywheel.GetDesiredVelocity();
     if (flywheelVelocity > decltype(flywheelVelocity){0})
     {
+        auto poseChange = pose.Translation() - m_previousPose.Translation();
+        auto dx = poseChange.X() / Constants::LoopPeriod;
+        auto dy = poseChange.Y() / Constants::LoopPeriod;
         auto flywheelTangentialVelocity = flywheelVelocity / 1_tr * Constants::Flywheel::Diameter * wpi::numbers::pi;
-        auto flightTime = goalDistance / (flywheelTangentialVelocity * units::math::cos(Constants::Flywheel::Angle));
+        auto flightTime =
+            goalOffset.Norm() / (flywheelTangentialVelocity * units::math::cos(Constants::Flywheel::Angle));
         // TODO: account for turret rotation
         goalOffset = goalOffset - frc::Translation2d{dx * flightTime, dy * flightTime};
     }
